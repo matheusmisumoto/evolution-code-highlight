9240eec153447ec61a8c88a331fde4581bda0348
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2023-12-27 20:03:54 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Highlight.js for Evolution
+
+This simple WordPress plugin imports a custom build of the [highlight.js](https://highlightjs.org/) library, a syntax highlighter written in JavaScript. It was written to be useful on blog posts of [my website](https://matheusmisumoto.dev/).
+
+For more information on highlight.js, please refer to its [documentation](https://highlightjs.readthedocs.io/en/latest/index.html).
+
+## Languages
+
+The custom build of highlight.js used in this repository supports the following syntaxes:
+
+- accesslog
+- bash
+- cmake
+- css
+- dart
+- dockerfile
+- gradle
+- haskell
+- ini
+- java
+- javascript
+- json
+- kotlin
+- makefile
+- nginx
+- php
+- powershell
+- properties
+- shell
+- sql
+- swift
+- typescript
+- yaml
+
+## Styling
+
+The CSS is based on the `VS2015` highlight.js theme. It has some minor adjustments to adapt to the custom Evolution theme for WordPress used on my website.
+
+---
+Developed with ☕ by [Matheus Misumoto](https://matheusmisumoto.dev) in Santos, Brazil
---
+++ evolution-code-highlight.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Plugin Name:       Highlight.js for Evolution
+ * Description:       Plugin to highlight code in your posts. Uses a custom build of highlight.js.
+ * Version:           1.0
+ * Requires at least: 6.0
+ * Requires PHP:      7.4
+ * Author:            Matheus Misumoto
+ * Author URI:        https://matheusmisumoto.dev/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'PLUGIN_NAME_VERSION', '1.0' );
+
+if ( ! function_exists( 'run_code_highlight' ) ) {
+    function run_code_highlight(){
+        wp_enqueue_style( 'highlight', plugin_dir_url( __FILE__ ) . 'highlightjs.css', array(), '11.9.0', 'all' );
+        wp_enqueue_script( 'highlight', plugin_dir_url( __FILE__ ) . 'highlight.min.js', array(), '11.9.0');
+        wp_add_inline_script('highlight', 'hljs.highlightAll();');
+    }
+}
+
+add_action('wp_enqueue_scripts', 'run_code_highlight');
+?>
