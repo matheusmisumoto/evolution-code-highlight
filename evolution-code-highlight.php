33602a73e8d09cb694e8f9764ea3c67640679a0e
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2024-01-30 01:33:30 -0300

    Refactor: change plugin version constant

--- evolution-code-highlight.php
+++ evolution-code-highlight.php
@@ -22,7 +22,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.1' );
+define( 'HIGHLIGHT_VERSION', '1.1' );
 
 if ( ! function_exists( 'run_code_highlight' ) ) {
     function run_code_highlight(){
